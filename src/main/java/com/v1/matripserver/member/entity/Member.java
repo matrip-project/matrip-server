cd54bf7b8085fc0daab5cbaa8fe0d1b10ac3dbd6
hapuripopo <sunmoon4228@gmail.com>
2023-11-22 23:07:16 +0900

    refactor: Member 엔티티 수정

--- src/main/java/com/v1/matripserver/member/entity/Member.java
+++ src/main/java/com/v1/matripserver/member/entity/Member.java
@@ -7,6 +7,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(callSuper = false)
 @Getter
@@ -14,32 +16,38 @@ import lombok.NoArgsConstructor;
 @Table(name = "member")
 public class Member extends BaseEntity {
     @Id
-    @Column(name = "id", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "password", nullable = false)
+    private String password;
+
     @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "birth", nullable = false)
+    private LocalDateTime birth;
 
     // M: male, F: female
     @Column(name = "sex", nullable = false)
     private char sex;
 
-    @Column(name = "age", nullable = false)
-    private int age;
+    @Column(name = "nation", nullable = false)
+    private String nation;
 
-    @Column(name = "intro", nullable = false)
+    @Column(name = "intro")
     private int intro;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "auth", nullable = false)
     private Auth auth;
-
-    @Column(name = "link", nullable = false)
-    private String link;
 
     public void setAuth(Auth auth) {
         this.auth = auth;
