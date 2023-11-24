a35d6c1fa687ea22da1ebc77886f0fea33dbf125
hapuripopo <sunmoon4228@gmail.com>
2023-11-25 02:40:05 +0900

    feat: 커스텀 예외 처리 추가

---
+++ src/main/java/com/v1/matripserver/util/exceptions/BaseResponseStatus.java
@@ -0,0 +1,34 @@
+package com.v1.matripserver.util.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum BaseResponseStatus {
+
+    // ----- Success Response -----
+    SUCCESS(true, HttpStatus.OK.value(), "요청에 성공하였습니다."),
+
+    // ----- Common Responses -----
+    COMMON_NOT_FOUND(false, HttpStatus.NOT_FOUND.value(), "정보를 가져오는데 실패하였습니다."),
+    COMMON_CREATE_FAILED(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "생성에 실패하였습니다."),
+    NO_REQUIRED_INFORMATION(false, HttpStatus.BAD_REQUEST.value(), "필수 입력 항목이 누락되었습니다."),
+    INVALID_INPUT_VALUE(false, HttpStatus.BAD_REQUEST.value(), "올바르지 않은 입력 값입니다."),
+    NAME_SUFFIX_LIMIT_EXCEEDED(false, HttpStatus.BAD_REQUEST.value(), "이름의 접미사가 제한 값을 초과하였습니다."),
+    UNEXPECTED_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "예상치 못한 에러가 발생했습니다."),
+
+    // ----- User-related Responses -----
+    DUPLICATED_EMAIL(false, HttpStatus.CONFLICT.value(), "이미 사용 중인 이메일입니다."),
+    DUPLICATED_NICKNAME(false, HttpStatus.CONFLICT.value(), "이미 사용중인 닉네임입니다.");
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
