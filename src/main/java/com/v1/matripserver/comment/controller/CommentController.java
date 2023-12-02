16a3fa5b39eb271cde1df01fb6d2b82fdc5d8aa7
youngsoosoo <lovepys01@naver.com>
2023-12-02 23:00:04 +0900

    feat: 댓글 삭제

--- src/main/java/com/v1/matripserver/comment/controller/CommentController.java
+++ src/main/java/com/v1/matripserver/comment/controller/CommentController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +41,13 @@ public class CommentController {
 
         List<CommentResponseDto> commentResponseDtoList = commentService.readComment(commentRequestDto);
         return ResponseEntity.ok(commentResponseDtoList);
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteComment(@PathVariable Long id){
+
+        commentService.deleteComment(id);
+        return ResponseEntity.ok().build();
     }
 }
