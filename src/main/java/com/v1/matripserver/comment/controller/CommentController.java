7cab6bd75d30d8d65a625a599370937d4c5fd1bf
youngsoosoo <lovepys01@naver.com>
2023-12-02 23:12:22 +0900

    feat: 댓글 수정

--- src/main/java/com/v1/matripserver/comment/controller/CommentController.java
+++ src/main/java/com/v1/matripserver/comment/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,13 @@ public class CommentController {
     public ResponseEntity<?> deleteComment(@PathVariable Long id){
 
         commentService.deleteComment(id);
+        return ResponseEntity.ok().build();
+    }
+
+    // 댓글 수정
+    @PutMapping("")
+    public ResponseEntity<?> updateComment(@RequestBody CommentRequestDto commentRequestDto){
+        commentService.updateComment(commentRequestDto);
         return ResponseEntity.ok().build();
     }
 }
