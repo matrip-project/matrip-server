44367c655aae04704f5d394ee1505a9737731e3f
youngsoosoo <lovepys01@naver.com>
2023-12-05 00:23:24 +0900

    fix: 상태 추가에 따른 조회 수정

--- src/main/java/com/v1/matripserver/journey/repository/JourneyRepository.java
+++ src/main/java/com/v1/matripserver/journey/repository/JourneyRepository.java
@@ -15,16 +15,16 @@ public interface JourneyRepository extends JpaRepository<Journey, Long> {
 
     // 동행 정보를 가지고 상품 이미지 정보(한 개)와 댓글의 개수를 구해주는 메서드 페이지 단위로 구하기
     @Query("select j.id, j.title, j.city, j.status, j.startDate, j.endDate, j.memberId, ji, count(j.id) from Journey j left outer join JourneyImg ji on ji.journeyId = j" +
-        " where ji.status = :journeyImgStatus and ji.sequence = 0"
+        " where (ji is null or (ji is not null and (ji.status = 'ACTIVE' and ji.sequence = 0)))"
         + " and (:keyword is null or j.title like %:keyword%)"
         + " and (:city is null or j.city = :city)"
-        + " and ((:journeyStatus is null and (j.status = 'ACTIVE' or j.status = 'CLOSED')) or j.status = :journeyStatus)"
+        + " and j.status = :journeyStatus"
         + " and ((:startDate is null and :endDate is null) or (j.startDate <= :startDate and j.endDate >= :endDate))"
-        + " and j.memberId.status = :journeyImgStatus"
+        + " and j.memberId.status = 'ACTIVE'"
         + " and ((:startYear=0 and :endYear=0) or (YEAR (j.memberId.birth) BETWEEN :startYear and :endYear))"
         + " group by j")
     Page<Object[]> readJourneyList(Pageable pageable, String keyword, String city,
-        LocalDate startDate, LocalDate endDate, Status journeyStatus, Status journeyImgStatus, Integer startYear, Integer endYear);
+        LocalDate startDate, LocalDate endDate, Status journeyStatus, Integer startYear, Integer endYear);
 
     // 하나의 동행 게시글 아이디를 가지고 이미지 정보를 구해주는 메서드
     @Query("select j, ji, count(c.id) from Journey j"
--- src/main/java/com/v1/matripserver/journey/service/JourneyService.java
+++ src/main/java/com/v1/matripserver/journey/service/JourneyService.java
@@ -97,8 +97,7 @@ public class JourneyService {
 
         // 검색할 시작 연도와 끝 연도 계산
         Page<Object []> result = journeyRepository.readJourneyList(pageable, pageRequestDTO.getKeyword(), pageRequestDTO.getCity(),
-            pageRequestDTO.getStartDate(), pageRequestDTO.getEndDate(), Status.valueOf(pageRequestDTO.getStatus()), Status.ACTIVE,
-            startYear, endYear);
+            pageRequestDTO.getStartDate(), pageRequestDTO.getEndDate(), Status.valueOf(pageRequestDTO.getStatus()), startYear, endYear);
 
         Function<Object [], JourneyResponseDto> fn = (arr -> {
             Journey journey = Journey.builder()
